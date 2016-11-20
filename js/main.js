432b964e40c06b7b2419afb0c22051478adc4813
Aleksei Piatetskyi <a.piatetskyi@gmail.com>
2016-11-21 00:42:54 +0200

    Stylized filter

--- js/main.js
+++ js/main.js
@@ -1,35 +1,3 @@
-var inputText = document.querySelectorAll(".form__input, .form__textarea"),
-    feedback = document.querySelector(".feedback"),
-    feedbackOpen = document.querySelector(".feedback__open"),
-    feedbackForm = feedback.querySelector(".feedback__form");
-    feedbackClose = feedback.querySelector(".feedback__close");
-
-    feedbackOpen.addEventListener("click", function(event){
-      event.preventDefault();
-      feedback.classList.add("feedback_show");
-      feedbackForm.classList.add("feedback__form_show");
-    })
-
-    feedbackClose.addEventListener("click", function(event){
-      event.preventDefault();
-      feedback.classList.remove("feedback_show");
-      feedbackForm.classList.remove("feedback__form_show");
-    })
-
-for (var i = 0; i < inputText.length; i++) {
-  inputText[i].addEventListener("blur", function(event) {
-    var inputLabel = this.nextElementSibling;
-    if (this.value) {
-      inputLabel.classList.add("form__input-label_fixed")
-    } else {
-      inputLabel.classList.remove("form__input-label_fixed")
-    }
-  })
-}
-
-
-ymaps.ready(init);
-
 function init() {
   var myMap = new ymaps.Map("map", {
       center: [59.9392, 30.329],
@@ -46,3 +14,41 @@ function init() {
 
   myMap.geoObjects.add(gllasyGeoPosition)
 }
+
+function feedback() {
+  var feedback = document.querySelector(".feedback"),
+    feedbackOpen = document.querySelector(".feedback__open"),
+    feedbackForm = feedback.querySelector(".feedback__form"),
+    feedbackClose = feedback.querySelector(".feedback__close");
+
+  feedbackOpen.addEventListener("click", function(event) {
+    event.preventDefault();
+    feedback.classList.add("feedback_show");
+    feedbackForm.classList.add("feedback__form_show");
+  })
+
+  feedbackClose.addEventListener("click", function(event) {
+    event.preventDefault();
+    feedback.classList.remove("feedback_show");
+    feedbackForm.classList.remove("feedback__form_show");
+  })
+}
+
+
+
+var inputText = document.querySelectorAll(".form__input, .form__textarea");
+
+
+for (var i = 0; i < inputText.length; i++) {
+  inputText[i].addEventListener("blur", function(event) {
+    var inputLabel = this.nextElementSibling;
+    if (this.value) {
+      inputLabel.classList.add("form__input-label_fixed")
+    } else {
+      inputLabel.classList.remove("form__input-label_fixed")
+    }
+  })
+}
+
+document.addEventListener("DOMContentLoaded", feedback);
+ymaps.ready(init);
